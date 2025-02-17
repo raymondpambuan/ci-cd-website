e7581f0aff4d546a567bc46931bd4f0f8afd76ee
raymondpambuan <raymondipambuan@gmail.com>
2025-02-18 01:18:33 +0800

    Added GitHub Actions (main.yml)

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "website_url" {
-    description = "URL of the website"
-    value = aws_s3_bucket_website_configuration.hosting_bucket_website_configuration.website_endpoint
+  description = "URL of the website"
+  value       = aws_s3_bucket_website_configuration.hosting_bucket_website_configuration.website_endpoint
 }
