f6a64de5618d2c7b205524ea1a519d9968e5a304
raymondpambuan <raymondipambuan@gmail.com>
2025-02-18 03:56:34 +0800

    Modified main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,10 +14,10 @@ resource "aws_s3_bucket_ownership_controls" "this" {
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  # block_public_acls       = false
+  # block_public_policy     = false
+  # ignore_public_acls      = false
+  # restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_acl" "example" {
